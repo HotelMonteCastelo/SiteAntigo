c3748d07ee112a34a457a8b931a76c5ad8f87af4
Michael Rocha Nunes <michaelrn2288@gmail.com>
2023-08-04 22:40:48 -0300

    creating collapsible menu

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ const menuController = document.querySelector('.menu-controller')
 const websiteShortcuts = document.querySelector('.website-shortcuts')
 
 
+menuController.addEventListener('click', ()=>{
+    if (websiteShortcuts.classList.contains('hidden')) {
+        websiteShortcuts.classList.remove('hidden')
+    }
+    else {
+        websiteShortcuts.classList.add('hidden')
+    }
+})
