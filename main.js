00c9bf62e74a0a4987a3c33d2bcdb74f43c268e2
Michael Rocha <contato@hotelmontecastelo.com>
2023-08-07 18:44:31 -0300

    improve style to wider screen and collapsible menu animation

--- main.js
+++ main.js
@@ -6,17 +6,23 @@ window.addEventListener('loadstart', renderMenuBasedOnScreenWidth())
 window.addEventListener('resize', renderMenuBasedOnScreenWidth)
 
 menuController.addEventListener('click', ()=>{
-    websiteShortcuts.classList.contains('hidden') ? showMenu() : hideMenu()
+    if (websiteShortcuts.classList.contains('visible-menu-options')){
+        hideMenu()
+    } else if (websiteShortcuts.classList.contains('hidden-menu-options')) {
+        showMenu()
+    }
     }
 )
 
 function showMenu() {
-    websiteShortcuts.classList.remove('hidden')
+    websiteShortcuts.classList.add('visible-menu-options')
+    websiteShortcuts.classList.remove('hidden-menu-options')
     menuController.textContent = 'X'
 }
 
 function hideMenu () {
-    websiteShortcuts.classList.add('hidden')
+    websiteShortcuts.classList.add('hidden-menu-options')
+    websiteShortcuts.classList.remove('visible-menu-options')
     menuController.textContent = 'MENU'
 }
 
