c41c2f52f93ba150e481a71bfb5397bcbc3ab150
Michael Rocha Nunes <michaelrn2288@gmail.com>
2023-08-06 22:41:28 -0300

    add style to welcome section and improve nav header on mobile screens

--- main.js
+++ main.js
@@ -2,14 +2,13 @@ const menuController = document.querySelector('.menu-controller')
 const websiteShortcuts = document.querySelector('.website-shortcuts')
 
 
+window.addEventListener('loadstart', renderMenuBasedOnScreenWidth())
+window.addEventListener('resize', renderMenuBasedOnScreenWidth)
+
 menuController.addEventListener('click', ()=>{
     websiteShortcuts.classList.contains('hidden') ? showMenu() : hideMenu()
     }
 )
-
-window.addEventListener('resize', ()=>{
-    window.innerWidth > 800 ? showMenu() : hideMenu()
-})
 
 function showMenu() {
     websiteShortcuts.classList.remove('hidden')
@@ -19,4 +18,8 @@ function showMenu() {
 function hideMenu () {
     websiteShortcuts.classList.add('hidden')
     menuController.textContent = 'MENU'
+}
+
+function renderMenuBasedOnScreenWidth() {
+    window.innerWidth > 800 ? showMenu() : hideMenu()
 }
